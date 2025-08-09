163bd5aa668827a7683b92b515c5715c3404cc5f
Dilip2005 <101985281+Dilip2005@users.noreply.github.com>
2025-08-09 18:53:20 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,6 +10,8 @@ test('has title', async ({ page }) => {
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
+  console.log("update From GitHub")
+
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
